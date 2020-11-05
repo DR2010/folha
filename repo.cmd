b3e8890d939079dbb0f1eb63ae30e6eca41a95b4
Daniel <daniellgmachado@gmail.com>
2020-11-06 08:45:50 +1100

    2nd commit

---
+++ repo.cmd
@@ -0,0 +1,10 @@
+echo "# folha" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/DR2010/folha.git
+git push -u origin main
+
+git add *
+git push
